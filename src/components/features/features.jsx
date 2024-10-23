ca7223fd8b1796794ee452b36f481ca4c27169a8
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2024-10-23 10:17:28 -1100

    Add files via upload

---
+++ src/components/features/features.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { image } from '../images';
+import './features.css'
+
+
+const Features = () => {
+  return (
+    <div className='features'> 
+    
+    <div className='image'>
+        <img src={image.feature_img} alt="" />
+    </div>
+
+
+    <div className='text'>
+        <h3>Features</h3>
+        <h1>Unlocking Audit Confidence</h1>
+        <ul>
+            <li>AuditaBlock ensures immutable audit records, safeguarding data 
+                integrity and preventing tampering. its real-time tracking 
+                capability provides instant visibility into audit processes, 
+                enhancing transparency and stakeholder confidence.</li>
+
+            <li>Automated compliance ensures adherence to regulatory standards, 
+                mitigating risks and penalties. Decentralized data storage
+                 protects sensitive information, guaranteeing accessibility and security</li>  
+
+
+            <li>AuditBlock’s smart contract technology streamlines audit workflows, 
+                minimizing manual errors. Advanced analytics 
+                provide actionable insights, 
+                informing strategic business decisions</li>
+        </ul>
+    </div>
+    </div>
+  )
+}
+
+export default Features;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+
+
+        <div>
+            <h1>AuditaBlock</h1>
+            <p>AuditaBlock Streamlines auditing processes, enhancing efficency, transparency and security</p>
+            <div className="icons">
+                <a href="#"><img src="" alt="" /></a>
+                <a href="#"><img src="" alt="" /></a>
+                <a href="#"><img src="" alt="" /></a>
+                <a href="#"><img src="" alt="" /></a>
+                <a href="#"><img src="" alt="" /></a>
+            </div>
+        </div>
+
+
+        <div>
+           
+           
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Footer
