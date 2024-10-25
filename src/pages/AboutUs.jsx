9240ff4b10354c421e72ddb88e1b377d16a97401
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2024-10-25 06:42:44 -1100

    Add files via upload

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import AboutUsBanner from '../components/aboutusbanner/AboutUsBanner'
 import Footer from '../components/footer/Footer'
-import Facts from '../components/Facts/facts'
-import Team from '../components/teammembers/team'
+import Facts from '../components/Facts/Facts'
+import Team from '../components/teammembers/Team'
 
 
 const AboutUs = () => {
