c78bfb648153333f4aea7a3ce6d7804d4e055bca
chiscookeke11 <chiscookeke11@gmail.com>
2024-10-27 19:25:25 -1100

    +

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage'
 import { Route, Routes } from 'react-router-dom'
 import AboutUs from './pages/AboutUs'
 import Navbar from './components/Navbar/Navbar'
+import Contact from './pages/Contact'
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path='/aboutus' element={<AboutUs/>} />
+        <Route path='/contact' element ={<Contact/>} />
        </Routes>
     </div>
   )
